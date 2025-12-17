5f3ad2eb70e6a1c09d531545cf7328d753e63518
vinayakbhat123 <abcdbhat014@gmail.com>
2025-12-17 22:50:16 +0530

    list added to collections

--- Files/collection.py
+++ Files/collection.py
@@ -3,10 +3,27 @@
 #set = {}  unordered and immutable.but ADD/REMOVE OK.No dublicates 
 #tuple = () ordered and unchangle Dublicates OK
 
-fruits =["apple","banana","mango","orange"]
-print(dir(fruits))
 
+
+         #List =[]
+
+#fruits =["apple","banana","mango","orange"]
+# print(dir(fruits))
+# print(help(fruits))
+# print(len(fruits))
 # print(fruits[0:3])
-
+# fruits[0] = "Grapes"
+# fruits.append("grapes")  =>   # ['apple', 'banana', 'mango', 'orange', 'grapes']
+# fruits.remove("orange") 
+# fruits.insert(3,"apple")
+# fruits.sort()
+# fruits.reverse()
+# fruits.clear()
+# print(fruits.index("orange"))
+# print(fruits.count("orange"))
+# print(fruits)
 # for i in fruits:
 #   print(i)
+
+
+        #set = {}
